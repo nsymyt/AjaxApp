e2c540aa2352e5b71fbbb9810f3186d69d118d58
nsymyt <otuyamayihsin.4@gmail.com>
2020-09-04 17:02:40 +0900

    Update memo.js

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -5,8 +5,6 @@ function memo() {
     const XHR = new XMLHttpRequest();
     XHR.open("POST", "/posts", true);
     XHR.responseType = "json";
-    console.log(formData)
-    console.log(XHR)
     XHR.send(formData);
     XHR.onload = () => {
       const item = XHR.response.post;
